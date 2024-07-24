7f9900e8c7d0a77b1e4b62bc970d267b24d080c0
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-24 07:39:55 +0000

    added data and classes to the new-form for choices

--- resources/views/choice/new-form.blade.php
+++ resources/views/choice/new-form.blade.php
@@ -3,11 +3,13 @@
 @section('title', 'Index page')
 
 @section('content')
-  <h1>New Choice for: {{$question_txt}}</h1>
-  <form action="{{ route('choices.store', $quest) }}" method="POST">
-    @csrf
-    <label for="choice">Choice</label>
-    <input type="text" id="choice" name="choice"/>
-    <input type="submit" value="Submit"/>
-  </form>
+  <div class="new-form-choice">
+    <h1 id="title">New Choice for: {{$question_txt}}</h1>
+    <form action="{{ route('choices.store', $quest) }}" method="POST" class="new-choice-form">
+      @csrf
+      <label for="choice">Choice</label>
+      <input type="text" id="choice" name="choice"/>
+      <input type="submit" value="Submit" class="btn submit-btn"/>
+    </form>
+  </div>
 @endsection
