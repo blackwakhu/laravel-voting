cd58a70a8fbcb7dcd4ee88da57a8cea0f2841efe
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-24 08:35:03 +0000

    finished with the detail page of the questions

--- resources/views/question/detail.blade.php
+++ resources/views/question/detail.blade.php
@@ -3,44 +3,53 @@
 @section('title', 'Question Detail')
 
 @section('content')
-
-  <h1>Question</h1>
-  <p>{{ $quest->question_text }}</p>
- 
-  <button class="hide-new-choice-btn btn">
-    New Choice
-  </button>
-  <div class="new-choice-detail">
-    <form action="{{ route('choices.store', $quest) }}" method="POST">
-      @csrf
-      <label for="choice">Choice</label>
-      <input type="text" id="choice" name="choice"/>
-      <input type="submit" value="Submit"/>
-    </form>
+  <div class="question-detail">
+    <h1 id="title">Question</h1>
+    <p id="question-text">{{ $quest->question_text }}</p>
+   
+    <button class="hide-new-choice-btn btn">
+      New Choice
+    </button>
+    <div class="new-choice-detail">
+      <form action="{{ route('choices.store', $quest) }}" method="POST" class="new-choice-detail-form">
+        @csrf
+        <label for="choice">Choice</label>
+        <input type="text" id="choice" name="choice"/>
+        <input type="submit" value="Submit"/>
+      </form>
+    </div>
+    <table class="question-detail-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Choice</th>
+          <th>Votes</th>
+          <th colspan="2">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+      @foreach ( $quest->choices as $choice )
+        <tr>
+          <td>{{ $loop->iteration }}</td>
+          <td>{{ $choice->choice_text }}</td>
+          <td>{{ $choice->votes }}</td>
+          <td>
+            <form method="POST" action="{{route('choices.vote', $choice)}}">
+              @csrf
+              @method('PUT')
+              <input type='submit' value='vote' class="btn submit-btn"/>
+            </form>
+          </td>
+          <td>
+            <button class="btn delete-btn">
+              <a href="{{ route('choices.delete', $choice) }}"> 
+                delete
+              </a>
+            </button>
+          </td>
+        </tr>
+      @endforeach
+      </tbody>
+    </table>
   </div>
-  <table>
-    <tbody>
-    @foreach ( $quest->choices as $choice )
-      <tr>
-        <td>{{ $loop->iteration }}</td>
-        <td>{{ $choice->choice_text }}</td>
-        <td>{{ $choice->votes }}</td>
-        <td>
-          <form method="POST" action="{{route('choices.vote', $choice)}}">
-            @csrf
-            @method('PUT')
-            <input type='submit' value='vote'/>
-          </form>
-        </td>
-        <td>
-          <button>
-            <a href="{{ route('choices.delete', $choice) }}"> 
-              delete
-            </a>
-          </button>
-        </td>
-      </tr>
-    @endforeach
-    </tbody>
-  </table>
 @endsection
