2d71e4f0ffa981a5ee2f506a7a777effeac72e16
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-24 07:09:16 +0000

    done with questions/index page adding css

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -3,41 +3,35 @@
 @section('title', 'All Questions')
 
 @section('content')
-  <ul>
-    <li><a href="{{ route('home') }}">Home</a></li>
-    <li><a href="{{ route('questions.create')}}">New Question</a></li>
-  </ul>
-  <h1>All the questions</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>id</th>
-        <th>Question</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($questions as $question)
-        <tr>
-          <td>{{ $question->id }}</td>
-          <td>
-            <a href="{{ route('questions.detail', $question) }}">
-              {{ $question->question_text }}
-            </a>
-          </td>
-          <td>
-            <a href="{{ route('choices.create', $question) }}">
-              New Choice
-            </a>
-          </td>
-          <td>
-            <button>
-              <a href="{{ route('questions.delete', $question)}}">
-                Delete
+  <div class="index-questions">
+    <h1 id="title">All the questions</h1>
+    <table class="index-questions-table">
+      <tbody>
+        @foreach ($questions as $question)
+          <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td class="index-question-table-title">
+              <a href="{{ route('questions.detail', $question) }}">
+                {{ $question->question_text }}
               </a>
-            </button>
-          </td>
-        </tr>
-      @endforeach
-    </tbody>
-  </table>
+            </td>
+            <td>
+              <button class="add-btn btn">
+                <a href="{{ route('choices.create', $question) }}">
+                  New Choice
+                </a>
+              </button>
+            </td>
+            <td>
+              <button class="delete-btn btn">
+                <a href="{{ route('questions.delete', $question)}}">
+                  Delete
+                </a>
+              </button>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @endsection
