a50f896e0e29d2f4704fba772205c4b90dc158dc
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-24 06:32:38 +0000

    added css to the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,23 +3,7 @@
 @section('title', 'Index page')
 
 @section('content')
-  <h1>Hello and welcome to the pollster application</h1>
-  <ul>
-    <li>
-      <a href="{{ route('questions.index')}}">
-        All the questions
-      </a>
-    </li>
-    <li>
-      <a href="{{ route('questions.create') }}">
-        Create Question
-      </a>
-    </li>
-    <hr/>
-    <li>
-      <a href="{{ route('choices.index') }}">
-        All Choices
-      </a>
-    </li>
-  </ul>
+  <div class="index-page">
+    <h1 id="title">Hello!!!!!<br> Welcome to the Pollster Application!</h1>
+  </div>
 @endsection
