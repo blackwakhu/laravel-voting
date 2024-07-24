746d7ee7673d4014b6c378e64be27500026a24b0
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-24 08:49:30 +0000

    finished with the index for choice page adding css

--- resources/views/choice/index.blade.php
+++ resources/views/choice/index.blade.php
@@ -3,26 +3,31 @@
 @section('title', 'All Choices')
 
 @section('content')
-  <a href="{{ route('home') }}">home</a>
-  <h1>All the choice</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>id</th>
-        <th>choice</th>
-        <th>question id</th>
-        <th>votes</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($choices as $choice)
+  <div class="index-choice">
+    <h1 id="title">All the choice</h1>
+    <table class="index-choice-table">
+      <thead>
         <tr>
-          <td>{{ $choice->id }}</td>
-          <td>{{ $choice->choice_text }}</td>
-          <td>{{ $choice->question->question_text }}</td>
-          <td>{{ $choice->votes }}</td>
+          <th>#</th>
+          <th>Choice</th>
+          <th>Question</th>
+          <th>Votes</th>
         </tr>
-      @endforeach
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        @foreach ($choices as $choice)
+          <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $choice->choice_text }}</td>
+            <td class="index-choice-table-a">
+              <a href="{{route('questions.detail', $choice->question)}}">
+                {{ $choice->question->question_text }}
+              </a>
+            </td>
+            <td>{{ $choice->votes }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @endsection
